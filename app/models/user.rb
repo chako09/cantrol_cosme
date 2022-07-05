e4590e050de9ecaf6454f38d5515cba859792068
chako09 <asa0908.v3u@gmail.com>
2022-07-05 23:42:31 +0900

    ADD:Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :role, presence: true
+
+  enum role: { general: 0, admin: 1 }
 end
