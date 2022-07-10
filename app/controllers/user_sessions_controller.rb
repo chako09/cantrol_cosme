5be54e87acd4f75e34cb7290ed16e38923a98a67
chako09 <asa0908.v3u@gmail.com>
2022-07-10 22:20:39 +0900

    ADD:ログイン用のルーティング設定とuser_sessionsコントローラの追加

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,20 @@
+class UserSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_back_or_to signup_path, success:'ログインしました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to login_path, success:'ログアウトしました'
+  end
+end
